197961167bca5676594eb80dff18be0e4cef71d2
Nemztran <dinhnam13052006@gmail.com>
2025-11-15 11:08:50 +0700

    Adding CompactDisc

--- AimsProject/src/com/hust/kstn/test/CompactDiscTest.java
+++ AimsProject/src/com/hust/kstn/test/CompactDiscTest.java
@@ -1,4 +1,18 @@
 package com.hust.kstn.test;
+import com.hust.kstn.models.CompactDisc;
+import com.hust.kstn.models.Track;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class CompactDiscTest {
+    public static void main(String[] args) {
+        Track track1 = new Track("Track one", 36);
+        Track track2 = new Track("Track two", 37);
+        Track track3 = new Track("Track three", 38);
+        CompactDisc disc = new CompactDisc("Disc 1th", "Category 1", 12, Arrays.asList(track1, track2));
+        disc.removeTrack(track1);
+        disc.removeTrack(track3);
+        disc.addTrack(track3);
+    }
 }
