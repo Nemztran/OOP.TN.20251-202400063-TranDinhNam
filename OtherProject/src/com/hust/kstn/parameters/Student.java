b2afc004fd5eeba18e569cbd2a2215c1e0f67dc6
Nemztran <dinhnam13052006@gmail.com>
2025-11-15 16:13:30 +0700

    Test parameters

--- OtherProject/src/com/hust/kstn/parameters/Student.java
+++ OtherProject/src/com/hust/kstn/parameters/Student.java
@@ -1,4 +1,22 @@
 package com.hust.kstn.parameters;
 
 public class Student {
+    private int studentId;
+    private String name;
+    public Student(int studentId, String name) {
+        this.studentId = studentId;
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getStudentId() {
+        return studentId;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
 }
